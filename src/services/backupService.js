5d7915d26ab09b1dfb58012b85152c6adfba78e7
juanft0306 <juanft0308@gmail.com>
2026-07-06 15:43:32 -0400

    Implement backup and restore functions in backupService

--- src/services/backupService.js
+++ src/services/backupService.js
@@ -1,1 +1,9 @@
-//vacio//
+import { hacerBackup, restaurarBackup } from '../config/database.js';
+
+export function crearBackup() {
+    hacerBackup();
+}
+
+export function restaurarBackup() {
+    restaurarBackup();
+}
