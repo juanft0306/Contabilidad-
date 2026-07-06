123473f7618c84258d74c866a07edb85c97e8e3d
juanft0306 <juanft0308@gmail.com>
2026-07-06 15:52:43 -0400

    Add rate routes for updating internet rates

--- src/routes/rateRoutes.js
+++ src/routes/rateRoutes.js
@@ -1,1 +1,8 @@
-//vacio//
+import express from 'express';
+import * as rateController from '../controllers/rateController.js';
+
+const router = express.Router();
+
+router.post('/actualizar', rateController.actualizarTasaInternet);
+
+export default router;
