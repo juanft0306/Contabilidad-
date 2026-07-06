4f6d13895bd3b00088e180253d062a065a103232
juanft0306 <juanft0308@gmail.com>
2026-07-06 13:29:03 -0400

    Add functions to manage rates in rateModel.js

--- src/models/rateModel.js
+++ src/models/rateModel.js
@@ -1,1 +1,25 @@
-//vacio//
+export function obtenerTasaVigente(tasas) {
+    for (const [nombre, info] of Object.entries(tasas)) {
+        if (info.valor > 0) return { nombre, valor: info.valor };
+    }
+    return null;
+}
+
+export function actualizarTasa(tasas, historico, nombre, valor, fecha) {
+    tasas[nombre] = { valor, ultima_actualizacion: fecha };
+    historico.push({ nombre, valor, fecha });
+}
+
+export function resetTasasDiario(tasas) {
+    const ahora = new Date();
+    const hoy4am = new Date(ahora);
+    hoy4am.setHours(4, 0, 0, 0);
+    for (const info of Object.values(tasas)) {
+        if (info.valor !== 0 && info.ultima_actualizacion) {
+            const ultima = new Date(info.ultima_actualizacion);
+            if (ultima < hoy4am) {
+                info.valor = 0;
+            }
+        }
+    }
+}
