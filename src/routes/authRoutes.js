f4a0f48899866120ed49d2bbc481aec12d3d5d1e
juanft0306 <juanft0308@gmail.com>
2026-07-06 15:53:35 -0400

    Implement authentication routes for verification and password change

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,1 +1,9 @@
-//vacio//
+import express from 'express';
+import * as authController from '../controllers/authController.js';
+
+const router = express.Router();
+
+router.post('/verify', authController.verificarAcceso);
+router.post('/change', authController.cambiarPassword);
+
+export default router;
