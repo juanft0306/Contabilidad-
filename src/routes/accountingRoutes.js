bfb0d62abab5a9ff79bc4bdd7596e123cfb83b49
juanft0306 <juanft0308@gmail.com>
2026-07-06 15:52:22 -0400

    Set up accounting routes with controller methods

--- src/routes/accountingRoutes.js
+++ src/routes/accountingRoutes.js
@@ -1,1 +1,9 @@
-//vacio//
+import express from 'express';
+import * as accountingController from '../controllers/accountingController.js';
+
+const router = express.Router();
+
+router.get('/balance', accountingController.obtenerBalance);
+router.get('/libro', accountingController.obtenerLibroDiario);
+
+export default router;
