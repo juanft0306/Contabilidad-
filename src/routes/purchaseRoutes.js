d4524a745a549d68e2c8ca745ea3837c9028d7b4
juanft0306 <juanft0308@gmail.com>
2026-07-06 15:51:43 -0400

    Set up purchase routes with POST endpoint

--- src/routes/purchaseRoutes.js
+++ src/routes/purchaseRoutes.js
@@ -1,1 +1,8 @@
-//vacio//
+import express from 'express';
+import * as purchaseController from '../controllers/purchaseController.js';
+
+const router = express.Router();
+
+router.post('/', purchaseController.registrarCompra);
+
+export default router;
