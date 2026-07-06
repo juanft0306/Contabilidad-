d54651aba00268d190ddf25f691c8c81866882bf
juanft0306 <juanft0308@gmail.com>
2026-07-06 16:01:06 -0400

    Add product code and RIF validation functions

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,1 +1,7 @@
-//vacio//
+export function validarCodigoProducto(codigo) {
+    return /^[A-Z]\d{3}$/.test(codigo);
+}
+
+export function validarRIF(rif) {
+    return /^[JVE]-?\d{8,9}-?\d{1}$/i.test(rif);
+}
