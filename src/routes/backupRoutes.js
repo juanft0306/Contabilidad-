5d8f75ea9215bf89f34f75e197934513f974f906
juanft0306 <juanft0308@gmail.com>
2026-07-06 15:54:19 -0400

    Implement backup routes for creating and restoring backups

--- src/routes/backupRoutes.js
+++ src/routes/backupRoutes.js
@@ -1,1 +1,9 @@
-//vacio//
+import express from 'express';
+import * as backupController from '../controllers/backupController.js';
+
+const router = express.Router();
+
+router.post('/crear', backupController.hacerBackup);
+router.post('/restaurar', backupController.restaurarBackup);
+
+export default router;
