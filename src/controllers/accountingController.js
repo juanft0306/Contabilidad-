aeed7f30b88ba9e170f932379bce9dad19dafa52
juanft0306 <juanft0308@gmail.com>
2026-07-06 15:45:49 -0400

    Add accounting service functions to controller

--- src/controllers/accountingController.js
+++ src/controllers/accountingController.js
@@ -1,1 +1,14 @@
-//vacuo//
+import * as accountingService from '../services/accountingService.js';
+
+export function obtenerBalance(req, res) {
+    const datos = req.app.locals.datos;
+    const balance = accountingService.obtenerBalance(datos);
+    res.json(balance);
+}
+
+export function obtenerLibroDiario(req, res) {
+    const datos = req.app.locals.datos;
+    const { tipo, pagina = 0, tamano = 5 } = req.query;
+    const result = accountingService.obtenerLibroDiarioPaginado(datos, tipo, parseInt(pagina), parseInt(tamano));
+    res.json(result);
+}
