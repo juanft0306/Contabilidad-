3098f838184107d4e8b66d4a776e6b87b60182f7
juanft0306 <juanft0308@gmail.com>
2026-07-06 15:53:09 -0400

    Update clientRoutes.js

--- src/routes/clientRoutes.js
+++ src/routes/clientRoutes.js
@@ -1,1 +1,13 @@
-//vacio//
+import express from 'express';
+import * as clientController from '../controllers/clientController.js';
+
+const router = express.Router();
+
+router.get('/', clientController.listarClientes);
+router.post('/', clientController.crearCliente);
+router.delete('/cliente/:id', clientController.eliminarClienteController);
+router.get('/proveedores', clientController.listarProveedores);
+router.post('/proveedores', clientController.crearProveedor);
+router.delete('/proveedor/:id', clientController.eliminarProveedorController);
+
+export default router;
