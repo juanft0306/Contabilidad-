32766e49b938b6e47c3157f3492655cbb2d3bc73
juanft0306 <juanft0308@gmail.com>
2026-07-06 15:50:47 -0400

    Set up product routes with CRUD operations

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,1 +1,10 @@
-//vacuo//
+import express from 'express';
+import * as productController from '../controllers/productController.js';
+
+const router = express.Router();
+
+router.get('/', productController.listarProductos);
+router.post('/', productController.agregarProducto);
+router.delete('/:nombre', productController.eliminarProducto);
+
+export default router;
